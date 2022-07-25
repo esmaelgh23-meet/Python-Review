6392ac5074ad4c14b4008f0ad476abab1bc7e023
esmaelgh23-meet <109899626+esmaelgh23-meet@users.noreply.github.com>
2022-07-25 18:35:24 +0300

    Create pythonrev.py

---
+++ pythonrev.py
@@ -0,0 +1,17 @@
+def my_function():
+  print("Hello from a function")
+  title1= input()
+  description1 = input()
+  likes1=0
+  dislikes11=0
+  thisdict = {
+  "title":title1
+  "description": description1
+  "likes":likes1 
+  "dislikes":dislikes1
+}
+print(thisdict["brand"])
+ my_function()
+ def create_student(name,year,gender):
+  student = {“Name”:name,”Year”:year,”Gender”:gender}
+  return student
