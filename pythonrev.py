3b16ef15a708572ae45dba96cfdc65ed13d333d5
esmaelgh23-meet <109899626+esmaelgh23-meet@users.noreply.github.com>
2022-07-28 17:51:38 +0300

    done lab

--- pythonrev.py
+++ pythonrev.py
@@ -1,17 +1,87 @@
-def my_function():
-  print("Hello from a function")
-  title1= input()
-  description1 = input()
-  likes1=0
-  dislikes11=0
-  thisdict = {
-  "title":title1
-  "description": description1
-  "likes":likes1 
-  "dislikes":dislikes1
-}
-print(thisdict["brand"])
- my_function()
- def create_student(name,year,gender):
-  student = {“Name”:name,”Year”:year,”Gender”:gender}
-  return student
+def create_youtube_video(title,description):
+  youtube_video = {"title":title,"description":description,"likes":0,"dislikes":0,"comments":{}}
+  return youtube_video
+
+
+def like (youtube_video):
+  if "likes" in youtube_video:
+    youtube_video["likes"]+=1
+    return youtube_video
+
+
+def dislike (youtube_video):
+  if "dislikes" in youtube_video:
+    youtube_video["dislikes"]+=1
+    return youtube_video
+
+def add_comment(youtube_video,username,comment_text):
+  youtube_video["comments"][username]= comment_text
+  return youtube_video
+
+new_video=create_youtube_video("messi goal","great player")
+new_video=add_comment(new_video,"Esmael","ghjfafjh")
+
+print (new_video)
+
+for x in range(495):
+  new_video=like(new_video)
+
+new_video=dislike(new_video)
+
+
+print (new_video)
+
+
+
+
+
+
+
+
+
+# comments=create_function{username:text}
+
+
+# if (likes in new_function)
+#     likes=likes+1
+#  else
+#      likes=null
+
+# #change student_1
+# def create_like(new_function,likes):
+#   student = {“Name”:name,”Year”:year,”Gender”:gender}
+#   return create_like
+
+# def create_dislike(new_function,likes):
+#   student = {“Name”:name,”Year”:year,”Gender”:gender}
+#   return create_like
+
+# def (new_function,likes):
+#   student = {“Name”:name,”Year”:year,”Gender”:gender}
+#   return create_like
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
